2d695257c82e551f1562fe37e4a792f02be44561
nur <altun.darwaza@gmail.com>
2025-02-15 19:25:11 -0500

    Initial Commit

---
+++ HowAreYou/HowAreYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowAreYouApp.swift
+//  HowAreYou
+//
+//  Created by Nur Khadr on 2025-02-15.
+//
+
+import SwiftUI
+
+@main
+struct HowAreYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
